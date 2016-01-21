5b9e3ad15905f8805de61d30c12e7055bd4b8f80
hrandr <hrandrianarijaona@excilys.com>
2016-01-21 15:50:33 +0100

    menu de choix recherche dans l'activité de visualisation

--- src/main/java/com/haja/tionaApp/PdfActivity.java
+++ src/main/java/com/haja/tionaApp/PdfActivity.java
@@ -3,7 +3,11 @@ package com.haja.tionaApp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.joanzapata.pdfview.PDFView;
@@ -29,10 +33,23 @@ public class PdfActivity extends Activity implements OnPageChangeListener {
     PDFView pdfView;
     @ViewById(R.id.nb_edit)
     EditText editText;
+    @ViewById(R.id.btn_fihirana)
+    Button btnFihirana;
+    @ViewById(R.id.btn_antema)
+    Button btnAntema;
+    @ViewById(R.id.btn_fihirana_fanampiny)
+    Button btnFF;
+    @ViewById(R.id.btn_liturgique)
+    Button btnLiturgie;
+    @ViewById(R.id.btn_chorale)
+    Button btnChorale;
+    @ViewById(R.id.search_type)
+    TextView searchType;
 
     Intent intent;
     String pdfName;
     Integer pageNumber = 1;
+    private int mode = 0; // pour le mode de recherche selon le tab : 0 => fihirana, 1 => ff, 2 => antema, 3 => lt, 4 default
 
     @AfterInject
     public void afterInject(){
@@ -51,6 +68,48 @@ public class PdfActivity extends Activity implements OnPageChangeListener {
 //            }
               display(pdfName, true);
         }
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (v.getId()){
+                    case R.id.btn_fihirana:
+                        mode = 0;
+                        searchType.setText("Fihirana");
+                        Log.d("haja", "fihirana");
+                        break;
+                    case R.id.btn_fihirana_fanampiny:
+                        mode = 1;
+                        searchType.setText("Fihirana Fanampiny");
+                        Log.d("haja", "ff");
+                        break;
+                    case R.id.btn_antema:
+                        mode = 2;
+                        searchType.setText("Antema");
+                        Log.d("haja", "antema");
+                        break;
+                    case R.id.btn_liturgique:
+                        mode = 3;
+                        searchType.setText("Liturgie");
+                        Log.d("haja", "lt");
+                        break;
+                    default:
+                        mode = 0;
+                        searchType.setText("Default");
+                        Log.d("haja", "default");
+                        break;
+
+                }
+
+            }
+        };
+
+        btnFF.setOnClickListener(onClickListener);
+        btnFihirana.setOnClickListener(onClickListener);
+        btnAntema.setOnClickListener(onClickListener);
+        btnChorale.setOnClickListener(onClickListener);
+        btnLiturgie.setOnClickListener(onClickListener);
 
     }
 
@@ -88,7 +147,23 @@ public class PdfActivity extends Activity implements OnPageChangeListener {
     @Click(R.id.btn_go)
     public void go(){
         if(!editText.getText().toString().isEmpty()){
-            pdfName = editText.getText().toString() + ".pdf";
+            pdfName = "";
+            switch(mode){
+                case 0:
+                    break;
+                case 1:
+                    pdfName = "ff";
+                    break;
+                case 2:
+                    pdfName = "antema";
+                    break;
+                case 3:
+                    pdfName = "lt";
+                    break;
+                default:
+                    break;
+            }
+            pdfName = pdfName + editText.getText().toString() + ".pdf";
             display(pdfName, true);
         }
         else{
